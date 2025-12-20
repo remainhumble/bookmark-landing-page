502557bbaf8642333e438b40cf1fa51703415513
Thiflan Silmi Kaffah <thiflan120699@gmail.com>
2025-12-21 10:42:41 +1300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./index.css";
+
+const App = () => {
+    return (
+        <div>
+        <h1>This headline will use Poppins.</h1>
+            <p>A clean and simple interface to organize your favorite websites. Open a new browser tab and see your sites load instantly. Try it for free.</p>
+        </div>
+    )
+}
+
+export default App
