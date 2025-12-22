52b241be5c89fe6bc13c746552075ea19070ab30
Thiflan Silmi Kaffah <thiflan120699@gmail.com>
2025-12-22 13:40:21 +1300

    Features Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import React from 'react'
-import "./index.css";
-import imgs from '../images/images.js';
+import Features from './Features.jsx';
+import Hero from './Hero.jsx';
 
 const App = () => {
     return (
         <div>
-            <img src={imgs.illustrationHero} alt="" />
-            <h1 className="text-3xl text-blue-950 font-medium">A Simple Bookmark Manager</h1>
-            <p className="text-gray-500 font-normal">A clean and simple interface to organize your favorite websites. Open a new browser tab and see your sites load instantly. Try it for free.</p>
-            <button className="bg-blue-600 text-white px-4 py-2 rounded-md">Get it on Chrome</button>
-            <button className="bg-grey-50 text-white px-4 py-2 rounded-md"><span className='text-blue-950'>Get it on Firefox</span></button>
+            <Hero />
+            <Features />
         </div>
     )
 }
---
+++ src/Features.jsx
@@ -0,0 +1,17 @@
+import "./index.css";
+
+const Features = () => {
+    return (
+        <>
+            <h2 className="text-blue-950 font-medium">Features</h2>
+            <p className="text-xs text-gray-500">Our aim is to make it quick and easy for you to access your favourite websites. Your bookmarks sync between your devices so you can access them on the go.</p>
+            <ul className="text-xs text-gray-500">
+                <li>Simple Bookmarking</li>
+                <li>Speedy Searching</li>
+                <li>Easy Sharing</li>
+            </ul>
+        </>
+    )
+}
+
+export default Features;
