88568cc409fdefa502885de3ad8204175b622485
Thiflan Silmi Kaffah <thiflan120699@gmail.com>
2025-12-29 22:50:44 +1300

    switching between tabs

--- src/Features.jsx
+++ src/Features.jsx
@@ -18,9 +18,15 @@ const Features = () => {
                     <p className="text-gray-400 py-3">Our aim is to make it quick and easy for you to access your favourite websites. Your bookmarks sync between your devices so you can access them on the go.</p>
                 </div>
                 <ul className="text-gray-500 flex sm:flex-row flex-col justify-center items-center mt-4 joined max-w-md mx-auto">
-                    <li className="w-full border-solid border-b-0 sm:border-b-[1.5px] border-t-[1.5px] sm:border-t-0 h-full py-2 hover:text-red-400">Simple Bookmarking</li>
-                    <li className="w-full border-solid border-[1.5px_0_1.5px_0] sm:border-b-[1.5px] sm:border-t-0 h-full py-2 hover:text-red-400">Speedy Searching</li>
-                    <li className="w-full border-solid sm:border-b-[1.5px] border-b-[1.5px] sm:border-t-0 h-full py-2 hover:text-red-400">Easy Sharing</li>
+                        {featuresObj.map((feature, index) => (
+                            <li
+                                key={index}
+                                className={`w-full border-solid border-b-0 sm:border-b-[1.5px] border-t-[1.5px] sm:border-t-0 h-full py-2 hover:text-red-400 ${activeTab === index ? "text-red-400" : ""}`}
+                                onClick={() => handleTabClick(index)}
+                            >
+                                {feature.title}
+                            </li>
+                        ))}
                 </ul>
                 {/* tabs */}
                 {<div className="feature-content rounded-md flex flex-col md:flex-row justify-center items-center mt-4 px-4 md:px-8 py-6">
