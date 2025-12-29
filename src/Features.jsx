b124922395e94ee53b9c5694666baf3cf0bbffc6
Thiflan Silmi Kaffah <thiflan120699@gmail.com>
2025-12-30 11:39:00 +1300

    feature blob adjustment

--- src/Features.jsx
+++ src/Features.jsx
@@ -18,19 +18,29 @@ const Features = () => {
                     <p className="text-gray-400 py-3">Our aim is to make it quick and easy for you to access your favourite websites. Your bookmarks sync between your devices so you can access them on the go.</p>
                 </div>
                 <ul className="text-gray-500 flex sm:flex-row flex-col justify-center items-center mt-4 joined max-w-md mx-auto">
-                        {featuresObj.map((feature, index) => (
-                            <li
-                                key={index}
-                                className={`w-full border-solid border-b-0 sm:border-b-[1.5px] border-t-[1.5px] sm:border-t-0 h-full py-2 hover:text-red-400 ${activeTab === index ? "text-red-400" : ""}`}
-                                onClick={() => handleTabClick(index)}
-                            >
-                                {feature.title}
-                            </li>
-                        ))}
+                    {featuresObj.map((feature, index) => (
+                        <li
+                            key={index}
+                            role="tab"
+                            tabIndex={activeTab === index ? 0 : -1}
+                            aria-selected={activeTab === index}
+                            className={`w-full border-solid border-b-gray-400 border-b-[.5px] border-t-[.5px] sm:border-t-0 h-full py-2 hover:text-red-400 ${activeTab === index ? "text-blue-950 border-b-red-400 border-b-[4.5px]" : ""}`}
+                            onClick={() => handleTabClick(index)}
+                            onKeyDown={(e) => {
+                                if (e.key === "Enter" || e.key === " ") {
+                                    handleTabClick(index);
+                                }
+                            }}
+                        >
+                            {feature.tabTitle}
+                        </li>
+
+                    ))}
                 </ul>
                 {/* tabs */}
-                {<div className="feature-content rounded-md flex flex-col md:flex-row justify-center items-center mt-4 px-4 md:px-8 py-6">
-                    <img src={featuresObj[activeTab].image} alt={featuresObj[activeTab].alt} />
+                {<div className="feature-content rounded-md flex flex-col md:flex-row justify-center items-center mt-4 lg:px-18 md:px-8 ">
+                    <div className="feature-blob"></div>
+                    <img src={featuresObj[activeTab].image} alt={featuresObj[activeTab].alt} className="w-52 my-6 sm:my-0" />
                     <div className="flex flex-col justify-center items-center md:items-start gap-4 max-w-md text-center md:text-left md:px-11">
                         <h2 className="text-blue-950 text-sm mt-6">{featuresObj[activeTab].title}</h2>
                         <p className="text-gray-400 text-xs">{featuresObj[activeTab].description}</p>
--- src/Hero.jsx
+++ src/Hero.jsx
@@ -5,12 +5,12 @@ const Hero = () => {
     return (
         <>
             <div className="flex flex-col md:flex-row-reverse justify-center items-center gap-4 p-2">
-             
-                    <img src={imgs.illustrationHero} alt="illustration-hero" />
-                   <div className="hero-img"></div>
+
+                <img src={imgs.illustrationHero} alt="illustration-hero" />
+                <div className="hero-blob"></div>
                 <div className="flex flex-col justify-center items-center md:items-start gap-4  text-center md:text-left md:ml-12">
                     <h1 className="text-blue-950 md:text-2xl">A Simple Bookmark Manager</h1>
-                    <p className="text-gray-500">A clean and simple interface to organize your favorite websites. Open a new browser tab and see your sites load instantly. Try it for free.</p>
+                    <p className="text-gray-500">A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites load instantly. Try it for free.</p>
                     <div className="flex flex-row justify-center gap-2 mt-2">
                         <button className="bg-blue-600 text-white p-2 rounded-sm font-medium shadow-xl border-blue-600 hover:bg-white hover:text-blue-600 border-2 hover:border-blue-600">Get it on Chrome</button>
                         <button className="bg-grey-50 text-gray-600 p-2 rounded-sm font-medium shadow-xl border-grey-50 hover:bg-white hover:text-gray-600 border-2 hover:border-gray-600">Get it on Firefox</button>
