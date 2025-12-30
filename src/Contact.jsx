323ff955edf244662e46290c9fe23c1abc82dcf5
Thiflan Silmi Kaffah <thiflan120699@gmail.com>
2025-12-30 20:24:50 +1300

    some more interactivity

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -11,14 +11,11 @@ const Contact = () => {
     };
 
     if (!validateEmail(email) && email.length > 0) {
-        document.querySelector('.error').style.display = 'block';
-        document.querySelector('input').style.borderColor = 'var(--color-red-400)';
-    } else if (validateEmail(email)) {
-        document.querySelector('.error').style.display = 'none';
-        document.querySelector('input').style.borderColor = 'white';
+        document.querySelector(".error").style.display = "block";
+        document.querySelector("input").style.borderColor = "var(--color-red-400)";
     } else {
-        document.querySelector('.error').style.display = 'none';
-        document.querySelector('input').style.borderColor = 'white';
+        document.querySelector(".error").style.display = "none";
+        document.querySelector("input").style.borderColor = "white";
     }
 
     const handleSubmit = (e) => {
@@ -30,7 +27,7 @@ const Contact = () => {
             <div className="flex flex-col justify-center items-center gap-1 pt-3 pb-9 px-0 bg-blue-600" id="contact">
                 <span className="joined py-3 text-white uppercase tracking-[.25em] text-[11px]">35,000+ already joined</span>
                 <h2 className="max-w-53 text-white font-bold text-center">Stay up-to-date with what we're doing</h2>
-                <form onSubmit={handleSubmit} className="mt-4 flex flex-col items-center">
+                <form className="mt-4 flex flex-col items-center">
                     <div className="flex flex-col w-full sm:flex-row gap-2">
                         <div className="relative bg-red-400 duration-300 rounded-[0.3125rem]">
                             <input
@@ -40,7 +37,7 @@ const Contact = () => {
                                 placeholder="Enter your email address"
                                 className="w-full border-2 border-white text-[16px] px-2 py-2 rounded-sm bg-white outline-none flex-1"
                             />
-                            <span className="error  w-full flex justify-start text-sm rounded-sm py-1 px-2 italic text-white" style={{ display: 'none' }}>
+                            <span className="error w-full flex justify-start text-sm rounded-sm py-1 px-2 italic text-white">
                                 Whoops, make sure it's an email
                             </span>
                         </div>
